02e556ec73ab5a787695ea25fce614b732fc3baa
kellyhandy31@gmail.com <kellyhandy31@gmail.comgit config --global user.name Robert-kelly19>
2025-01-01 21:39:39 +0100

    instead of changing the bg-color for the theme i used brightness

--- script/script.js
+++ script/script.js
@@ -2,18 +2,19 @@ const light = document.getElementById('light')
 const dark = document.getElementById('dark')
 const btn = document.getElementById('btn')
 light.addEventListener('click', () => {
-  document.body.style.backgroundColor='black'
+  document.body.style.backdropFilter='brightness(1)'
   document.body.style.color='white'
   dark.style.display='flex'
   light.style.display='none'
   btn.style.border='solid white 1px'
 
 })
-
+  
 dark.addEventListener('click', () => {
-  document.body.style.backgroundColor='white'
+  document.body.style.backdropFilter='brightness(2)'
   document.body.style.color='black'
   light.style.display='flex'
   light.style.marginLeft='0px'
+  dark.style.display='none'
   btn.style.border='solid black 1px'
 })
